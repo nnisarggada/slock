0f33f352054e5118081ff46a1040bb7c4e55a923
Nnisarg Gada <gadannisarg@gmail.com>
2024-12-05 02:28:04 +0530

    init

---
+++ config.def.h
@@ -0,0 +1,18 @@
+/*Font settings for the time text*/
+static const float textsize=64.0;
+static const char* textfamily="serif";
+static const double textcolorred=255;
+static const double textcolorgreen=255;
+static const double textcolorblue=255;
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#1D2021",     /* after initialization */
+	[INPUT] =  "#fabd2f",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
